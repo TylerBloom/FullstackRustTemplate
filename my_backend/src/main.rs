d30be991b68a621239c67335c8c073f56be642b3
TylerBloom <tylerbloom2222@gmail.com>
2022-09-25 15:13:39 -0400

    Added tournament support to the backend

--- my_backend/src/main.rs
+++ my_backend/src/main.rs
@@ -10,29 +10,104 @@
 )]
 #![warn(rust_2018_idioms)]
 
-use my_sdk::hello::HelloWorldResponse;
-use rocket::{get, routes, Build, Rocket};
+use std::collections::HashMap;
+
+use once_cell::sync::OnceCell;
+use rocket::{get, post, routes, serde::json::Json, Build, Rocket};
+
+use my_sdk::{
+    model::{Tournament, TournamentId},
+    CreateTournamentRequest, CreateTournamentResponse, GetAllTournamentsResponse,
+    GetTournamentResponse, HelloWorldResponse, PerformActionRequest, PerformActionResponse,
+};
+use tokio::sync::RwLock;
+use uuid::Uuid;
 
 //#[cfg(test)]
 //mod tests;
 
+static TOURNAMENTS: OnceCell<RwLock<HashMap<TournamentId, Tournament>>> = OnceCell::new();
+
 #[get("/hello/<name>")]
 /// A simple "hello world" endpoint
 pub fn hello_world(name: String) -> HelloWorldResponse {
-    HelloWorldResponse::new(format!("Hello {name}!! Welcome to the world of fullstack Rust development!!"))
+    HelloWorldResponse::new(format!(
+        "Hello {name}!! Welcome to the world of fullstack Rust development!!"
+    ))
+}
+
+#[get("/get/<id>")]
+/// Returns a tournament, given an ID
+pub async fn get_tournament(id: Uuid) -> GetTournamentResponse {
+    GetTournamentResponse::new(
+        TOURNAMENTS
+            .get()
+            .unwrap()
+            .read()
+            .await
+            .get(&id.into())
+            .cloned(),
+    )
+}
+
+#[get("/get/all")]
+/// Returns all tournaments
+pub async fn get_all_tournaments() -> GetAllTournamentsResponse {
+    GetAllTournamentsResponse::new(TOURNAMENTS.get().unwrap().read().await.clone())
+}
+
+#[allow(unused_variables)]
+#[post("/create", format = "json", data = "<data>")]
+/// Creates a new tournament and returns in
+pub async fn create_tournament(data: Json<CreateTournamentRequest>) -> CreateTournamentResponse {
+    let id = Uuid::new_v4().into();
+    let tourn = Tournament::new();
+    TOURNAMENTS
+        .get()
+        .unwrap()
+        .write()
+        .await
+        .insert(id, tourn.clone());
+    CreateTournamentResponse::new((id, tourn))
+}
+
+#[post("/<id>/action", format = "json", data = "<data>")]
+/// Performs an action on a tournament
+pub async fn perform_tournament_action(
+    id: Uuid,
+    data: Json<PerformActionRequest>,
+) -> PerformActionResponse {
+    PerformActionResponse::new(
+        TOURNAMENTS
+            .get()
+            .unwrap()
+            .write()
+            .await
+            .get_mut(&id.into())
+            .map(|tourn| tourn.perform_action(data.0)),
+    )
 }
 
 /// Creates our Rocket!!
 pub fn init() -> Rocket<Build> {
     rocket::build()
+        .mount("/api/v1/", routes![hello_world])
         .mount(
-            "/api/v1/",
-            routes![hello_world],
+            "/api/v1/tournament",
+            routes![
+                get_tournament,
+                get_all_tournaments,
+                create_tournament,
+                perform_tournament_action
+            ],
         )
 }
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
+    TOURNAMENTS
+        .set(RwLock::new(HashMap::new()))
+        .expect("Could not set TOURNAMENTS");
     let client = init();
     // We don't need to use the rocket directly... yet
     let _rocket = client.launch().await?;
--- my_sdk/src/lib.rs
+++ my_sdk/src/lib.rs
@@ -12,11 +12,14 @@
 )]
 #![warn(rust_2018_idioms)]
 
+use std::collections::HashMap;
+
 /// From the users' side (namely the front and back ends), this module contains everything that the
 /// business logic library contains and renames it to `model`.
 pub use my_lib as model;
 
-use model::{Tournament, TournamentAction, TournamentResult};
+use model::{Tournament, TournamentAction, TournamentResult, TournamentId};
+use serde::{Serialize, Deserialize};
 
 /// Contains the wrapper response type
 pub mod response;
@@ -26,11 +29,16 @@ use crate::response::BasicResponse;
 /// The reponse type used by `api/v1/hello/<name>`
 pub type HelloWorldResponse = BasicResponse<String>;
 
-#[derive(Debug)]
+/// The reponse type used by `api/v1/tournament/get/<id>`
+pub type GetTournamentResponse = BasicResponse<Option<Tournament>>;
+/// The reponse type used by `api/v1/tournament/get/all`
+pub type GetAllTournamentsResponse = BasicResponse<HashMap<TournamentId, Tournament>>;
+
+#[derive(Debug, Serialize, Deserialize)]
 /// The request type used by `api/v1/tournament/create`
 pub struct CreateTournamentRequest;
 /// The request type used by `api/v1/tournament/create`
-pub type CreateTournamentResponse = BasicResponse<Tournament>;
+pub type CreateTournamentResponse = BasicResponse<(TournamentId, Tournament)>;
 
 /// The request type used by `api/v1/tournament/<id>/action`
 pub type PerformActionRequest = TournamentAction;
